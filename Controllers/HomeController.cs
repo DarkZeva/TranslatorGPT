b30db11af820c715f4f557a7068dbbdb7f77b01b
Mateusz Nowsadko <darkzeva@o2.pl>
2023-11-07 15:17:27 +0100

    Post working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 using TranslateGPT.Models;
 
@@ -7,20 +8,43 @@ namespace TranslateGPT.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+        private readonly List<string> mostUsedLanguages = new List<string>()
+        {
+            "Polish",
+            "English",
+            "German",
+            "Japanese",
+            "Korean",
+            "Turkish",
+        };
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, HttpClient httpClient)
         {
             _logger = logger;
+            _configuration = configuration;
+            _httpClient = httpClient;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Languages = new SelectList(mostUsedLanguages);
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OpenAIGPT(string query, string selectedLanguage)
+        {
+            var openAPIKey = _configuration["OpenAI:ApiKey"];
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {openAPIKey}");
+
+            return Ok();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
